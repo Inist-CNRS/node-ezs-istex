2aff62c7ffe242351a4566438a5e104fc066038c
François Parmentier <francois.parmentier@gmail.com>
2017-04-07 10:54:44 +0200

    :shirt: Lint

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-module.exports =  {
+/* eslint global-require: "off"*/
+module.exports = {
   ISTEXCorpus: require('./lib/corpus.js'),
   ISTEXQuery: require('./lib/query.js'),
   ISTEXHarvest: require('./lib/harvest.js'),
   ISTEXRequest: require('./lib/request.js'),
   ISTEXHits: require('./lib/hits.js'),
   ISTEXMods: require('./lib/mods.js'),
-  ISTEXParseXML: require('./lib/parse-xml.js')
-}
+  ISTEXParseXML: require('./lib/parse-xml.js'),
+};
